4d52b015c99b53b4d61e94a56674e893071001b5
dario-castillo <dariocas1905@gmail.com>
2023-06-13 23:20:50 -0300

    No me cargó el html

--- Proyecto/AppDario/urls.py
+++ Proyecto/AppDario/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
-from .views import saludo
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', saludo)
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
--- Proyecto/Proyecto/urls.py
+++ Proyecto/Proyecto/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AppDario/', include('AppDario.urls'))
+    path('', include('AppDario.urls'))
 ]

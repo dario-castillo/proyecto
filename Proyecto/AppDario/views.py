9f3f58eed8110a6050020b4153a770a00705d49d
dario-castillo <dariocas1905@gmail.com>
2023-06-23 03:29:24 -0300

    Formularios

--- Proyecto/AppDario/views.py
+++ Proyecto/AppDario/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Tarifario
+from .models import Newsletters
 
 def saludo(request):
    # return HttpResponse("Hola Darío")
@@ -7,8 +9,20 @@ def saludo(request):
     return render(request, "AppDario/index.html")
  
 def tarifario(request):
+    
+    if request.method == 'POST':
+        tarifario = Tarifario(nombre=request.POST['nombre'], apellido=request.POST['apellido'], email=request.POST['email'])
+        tarifario.save()    
+        return render(request, "AppDario/index.html")
+    
     return render(request, "AppDario/tarifario.html")
 
 def newsletter(request):
+    if request.method == 'POST':
+        newsletters = Newsletters(nombre=request.POST['nombre'], email=request.POST['email'])
+        newsletters.save()    
+        return render(request, "AppDario/index.html")
+
+
     return render(request, "AppDario/newsletter.html")
 
